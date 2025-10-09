1e77f03cc1adffb90ed61dac198548b5d46247b6
Jesus-Cabrera-Avila <24308060610607@cetis61.edu.mx>
2025-10-09 08:15:20 -0500

    si se pudo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Jesus Cabrera Avila
+## Programacion
+### 3-D
+#### CETis 61
+##### 24308060610607@cetis61.edu.mx
--- app.py
+++ app.py
@@ -3,24 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def base():
-    return render_template("base.html")
+def index():
+    return render_template("index.html")
 
-@app.route('/animales_exoticos')
-def animales():
-    return render_template('animales.html')
 
-@app.route('/carros_clasicos')
+@app.route("/animales")
+def animalia():
+    return render_template("animales.html")
+
+@app.route("/Carros")
 def carros():
-    return render_template('carros.html')
+    return render_template("carros.html")
 
-@app.route('/maravillas_del_mundo')
+@app.route("/maravillas")
 def maravillas():
-    return render_template('7_maravillas.htnl')
+    return render_template("7_maravillas.html")
 
-@app.route('/acerca_de')
+@app.route("/acerca_de")
 def acerca():
-    return render_template('acerca.html')
+    return render_template("acerca.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
