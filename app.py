ce52058ccae98dcb74cae9261ddb4665c4208976
Jesus-Cabrera-Avila <24308060610607@cetis61.edu.mx>
2025-10-22 08:37:11 -0500

    listo

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-
-@app.route("/inicia secion")
-def registro():
-    return render_template("registro.html")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("//iniciasecion", methods=["GET", "POST"])
+def registro():
+    error = None  
+
+    if request.method == "POST":
+        nombre = request.form.get("nombre")
+        apellido = request.form.get("apellido")
+        dia = request.form.get("dia")
+        mes = request.form.get("mes")
+        año = request.form.get("año")
+        genero = request.form.get("genero")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirmPassword")
+
+        if password != confirm_password:
+            error = "Las contraseñas no coinciden."
+        elif not nombre or not apellido or not email or not password or not confirm_password:
+            error = "Todos los campos son obligatorios."
+
+        if not error:
+            return redirect(url_for("index"))
+
+    return render_template("registro.html", error=error)
+
+
 
 @app.route("/animales")
 def animalia():
@@ -26,6 +48,7 @@ def maravillas():
 @app.route("/acerca_de")
 def acerca():
     return render_template("acerca.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
